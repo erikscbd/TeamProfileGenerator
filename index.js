60ec7d8fb734d22fdae1ad4c83956d1a3962c27b
erik escobedo <erikscbd@gmail.com>
2022-07-16 16:53:39 -0500

    prompt questions

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
-// Ask the question to get empolyee details
+const inq = require("inquirer");
+const { listenerCount } = require("process");
 
+const promptForNextEmployee = () => {
+    inq.prompt([{
+        message: 'Who would you like to add to your team?',
+        type: 'list',
+        choices: ['Intern', 'Engineer', 'Done'],
+        name: 'nextOperation'
+    
+}])
 
-// Create clases for Employee, Intern, Engineer, Manager
+.then(answers => {
+    switch (answers.nextOperation) {
+        case 'Intern': {
+            promptForIntern();
+            break;
+    }
+        case 'Engineer': {
+            promptForEngineer();
+            break;
+    }
+        case 'Done': {
+            console.log('Thanks for adding your team!');
+            break;
+    }
 
-
-//Create our HTML
-
-const inq = require('inquirer');
-
-inq.prompt([{
-    message: 'What is your name?',
-    type: 'input',
-    name: 'firstName'
-},
-{
-    message: 'What is your employye ID?',
-    type: 'input',
-    name: 'employeeId'
-},
-{
-    message: 'What is your email address?',
-    type: 'input',
-    name: 'email'
-},
-{
-    message: 'What is your office number?',
-    type: 'input',
-    name: 'officeNumber'
-},
-]).then((answers) => {
-    console.log(answers);
+        
+    }
 })
+
+inq.prompt([
+    {
+      message: "What is your name?",
+      type: "input",
+      name: "firstName",
+    },
+    {
+      message: "What is your employye ID?",
+      type: "input",
+      name: "employeeId",
+    },
+    {
+      message: "What is your email address?",
+      type: "input",
+      name: "email",
+    },
+    {
+      message: "What is your office number?",
+      type: "input",
+      name: "officeNumber",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+  });
+}
+
+
+  
